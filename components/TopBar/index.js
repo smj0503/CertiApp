32f55f10f7ac2f0f702512267f601a047fd6334c
smj0503 <wovj0503@gmail.com>
2023-11-28 16:22:11 +0900

    [Architecture : Sony] 초기 세팅

--- components/TopBar/index.js
+++ components/TopBar/index.js
@@ -2,14 +2,29 @@ import Link from "next/link";
 
 import styles from "./TopBar.module.css";
 import Logo from "../../public/assets/logo/logo-dashboard.svg";
+import IconLogout from "../../public/assets/icon-logout.svg";
 
-export default function ()
+export default function ({ children })
 {
     return (
         <div className={ styles.backgroundContainer }>
             <div className={ styles.topBar }>
+                <div className={ styles.menus }>
+                    <Logo/>
+                    <div>
+                        <Link href="/certificates" className={ styles.linkbutton }>{ "MY CERTIFICATE" }</Link>
+                        <Link href="/programs" className={ styles.linkbutton }>{ "OTHER PROGRAMS" }</Link>
+                    </div>
+                </div>
 
+                <button type="button" className={ styles.logout }>
+                    { "Log out" }<IconLogout/>
+                </button>
             </div>
+
+            <main className={ styles.body }>
+                { children }
+            </main>
         </div>
     )
 }
---
+++ pages/programs.js
@@ -0,0 +1,14 @@
+import TopBar from "@/components/TopBar";
+
+import styles from "../styles/Programs.module.css";
+
+export default function ()
+{
+    return (
+        <TopBar>
+            <div className={ styles.container }>
+
+            </div>
+        </TopBar>
+    )
+}
