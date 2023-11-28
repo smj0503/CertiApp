2e5a6320d22c90dae7f4b089bed45e8679aefb3d
smj0503 <wovj0503@gmail.com>
2023-11-28 13:37:53 +0900

    [Architecture : Sony] 초기 세팅

---
+++ components/TopBar/index.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+import styles from "./TopBar.module.css";
+import Logo from "../../public/assets/logo/logo-dashboard.svg";
+
+export default function ()
+{
+    return (
+        <div className={ styles.backgroundContainer }>
+            <div className={ styles.topBar }>
+
+            </div>
+        </div>
+    )
+}
