958d937d640418f3b688320c39e9af10be3d516b
smj0503 <wovj0503@gmail.com>
2023-11-29 19:59:27 +0900

    [Feature : Sony] Klip 로그인 연동 작업 중

---
+++ apis/klip.signin.api.js
@@ -0,0 +1,58 @@
+// import axios from "axios";
+//
+// export const getAddress = async () =>
+// {
+//     try
+//     {
+//         const { data } = await axios.post("https://a2a-api.klipwallet.com/v2/a2a/prepare", { bapp: { name: 'nftime' }, type: 'auth' });
+//
+//         return data;
+//     }
+//     catch (error)
+//     {
+//         console.log('error : ', error);
+//         return error;
+//     }
+// }
+
+//klip_test.js
+
+import axios from "axios";
+const A2P_API_PREPARE_URL = "https://a2a-api.klipwallet.com/v2/a2a/prepare";	//prepare url
+const APP_NAME = "GIMMEDUCK";
+
+//QR 생성 링크 만드는 함수
+const getKlipAccessUrl = (request_key) => {
+    return `https://klipwallet.com/?target=/a2a?request_key=${request_key}`;
+};
+
+//지갑 주소 수집
+export const getAddress = (setQrvalue, callback) => {
+    axios
+        .post(A2P_API_PREPARE_URL, {    //prepare
+            bapp: {
+                name: APP_NAME,
+            },
+            type: "auth",	//prepare 단계에서 인증 작업 요구
+        })
+        .then((response) => {   //request
+            const { request_key } = response.data;	//prepare 단계의 결과로request key 받음
+            setQrvalue(getKlipAccessUrl(request_key));    //QR code 생성
+            let timerId = setInterval(() => {
+                axios
+                    .get(   //result
+                        `https://a2a-api.klipwallet.com/v2/a2a/result?request_key=${request_key}`
+                    )
+                    .then((res) => {
+                        if (res.data.result) {
+                            console.log(`[Result] ${JSON.stringify(res.data.result.klaytn_address)}`);    //result에서 받은 결과 값 중 지갑 주소 확인
+                            console.log(res.data.result);
+                            console.log(res.data);
+                            callback(res.data.result.klaytn_address);
+                            clearInterval(timerId);
+                            setQrvalue("DEFAULT");
+                        }
+                    });
+            }, 1000);
+        });
+};
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import * as KlipAPI from "@/apis/klip.signin.api";
+import QRCode from "qrcode.react";
+
 import styles from "../styles/Login.module.css";
 import Logo from "../public/assets/logo/logo-homepage.svg";
 import IconHandShake from "../public/assets/icon-hand-shake.svg";
@@ -5,8 +9,17 @@ import IconKlip from "../public/assets/icon-klip.svg";
 
 export default function ()
 {
+    // const [url, setUrl] = useState('');
+    const [qrvalue_auth, setQrvalue_auth] = useState('');
+    const [myAddress, setMyAddress] = useState('');
+
     const onClick = async () =>
     {
+        // const { request_key } = await getAddress();
+        // setUrl(`https://klipwallet.com/?target=/a2a?request_key=${request_key}`);
+        KlipAPI.getAddress(setQrvalue_auth, async (address) => {
+            setMyAddress(address);	//사용자의 지갑 주소를 가져온다
+        });
     };
 
     return (
@@ -22,10 +35,16 @@ export default function ()
                     <h1 className={ styles.slogan }>{ "Remember that moment" }</h1>
                 </div>
                 <p dangerouslySetInnerHTML={{ __html: "you can easily and conveniently manage<br/>certificates such as diploma or awards" }} className={ styles. description }/>
-                <button type="button" className={ styles.loginButton } onClick={ onClick }>
-                    <IconKlip/>
-                    <label className={ styles.buttonLabel }>{ "Sign in with Klip" }</label>
-                </button>
+                {
+                    qrvalue_auth ? (
+                        <div style={{ marginTop: 48 }}><QRCode value={ qrvalue_auth } size={188}/></div>
+                    ): (
+                        <button type="button" className={ styles.loginButton } onClick={ onClick }>
+                            <IconKlip/>
+                            <label className={ styles.buttonLabel }>{ "Sign in with Klip" }</label>
+                        </button>
+                    )
+                }
             </div>
         </div>
     )
