439d0efabf7871112e5c3bc670d68705c7c6cd1c
smj0503 <wovj0503@gmail.com>
2023-12-20 17:44:24 +0900

    [Feature : Sony] http 통신

--- pages/[address]/archive/index.js
+++ pages/[address]/archive/index.js
@@ -5,70 +5,19 @@ import { useState } from "react";
 import TopBar from "@/components/TopBar";
 import SortButton from "@/components/SortButton";
 import Collection from "@/components/Collection";
-import Toast from "@/components/Toast/Share";
 
 import styles from "../../../styles/MyCertificate.module.css";
+import EmptyContainer from "@/components/EmptyContainer";
 
 export default function ()
 {
     const { t } = useTranslation("common");
     const router = useRouter();
 
-    const [isCopied, setIsCopied] = useState(false);
-
-    const close = () =>
-    {
-        setIsCopied(false);
-    };
-
     return (
         <TopBar hasSession={false}>
             <div className={ styles.container }>
-                <div>
-                    <div className={ styles.controller }>
-                        <div className={ styles.header }>
-                            <div>
-                                <span className={ styles.title }>{ t("myCertificates.belongsTo", { username: "Minki Park" }) }</span>
-                                <span className={ styles.count }>{ t("myCertificates.count", { count: 3 }) }</span>
-                            </div>
-                        </div>
-                        <div className={ styles. category }>
-                            <SortButton>{ t("myCertificates.all") }</SortButton>
-                            <SortButton>{ t("myCertificates.diploma") }</SortButton>
-                            <SortButton>{ t("myCertificates.contest") }</SortButton>
-                            <SortButton>{ t("myCertificates.license") }</SortButton>
-                            <SortButton>{ t("myCertificates.others") }</SortButton>
-                        </div>
-                        <Toast show={ isCopied } close={ close }/>
-                    </div>
-
-                    <div className={ styles.certificates }>
-                        <Collection
-                            image="/assets/photo/photo-certificate-1.png"
-                            href={ `${router.asPath}/ProtocolCamp2nd` }
-                            category="Diploma"
-                            date="2022.09.06"
-                            publisher="Hanhwa Dreamplus X Hashed"
-                            title="Protocol Camp 2nd"
-                        />
-                        <Collection
-                            image="/assets/photo/photo-certificate-2.png"
-                            href="https://honamict.kr/front/M0000151/program/programRequest.do?pgmId=PM000091"
-                            category="Diploma"
-                            date="2021.08.30"
-                            publisher="Gwangju ICT Innovation Square · Goorm"
-                            title="[ICT Innovation] Blockchain Education"
-                        />
-                        <Collection
-                            image="/assets/photo/photo-certificate-3.png"
-                            href="https://honamict.kr/front/M0000151/program/programRequest.do?pgmId=PM000091"
-                            category="Contest"
-                            date="2023.09.05"
-                            publisher="GICON"
-                            title="Digital Region Problem Solving Contest"
-                        />
-                    </div>
-                </div>
+                <EmptyContainer hasSession={false}/>
             </div>
         </TopBar>
     );
--- pages/[address]/index.js
+++ pages/[address]/index.js
@@ -30,14 +30,14 @@ export default function ()
     const { getMyCertificateList } = useCertificateModule();
 
     /* Life Cycle */
-    asyncEffect(async () =>
-    {
-        const result = await getUsername("0x2716150Ceb632D6a3C46DC5F577E0B1248CB008c");
-        setName(result.user_name)
-
-        const myCertificates = await getMyCertificateList("0x2716150Ceb632D6a3C46DC5F577E0B1248CB008c");
-        setMyCertificates(myCertificates);
-    }, []);
+    // asyncEffect(async () =>
+    // {
+    //     const result = await getUsername("0x2716150Ceb632D6a3C46DC5F577E0B1248CB008c");
+    //     setName(result.user_name)
+    //
+    //     const myCertificates = await getMyCertificateList("0x2716150Ceb632D6a3C46DC5F577E0B1248CB008c");
+    //     setMyCertificates(myCertificates);
+    // }, []);
 
     /* User Actions */
     const share = () =>
@@ -53,43 +53,44 @@ export default function ()
     return (
         <TopBar hasSession={true}>
             <div className={ styles.container }>
-                {
-                    myCertificates.length > 0 ? (
-                        <div>
-                            <div className={ styles.controller }>
-                                <div className={ styles.header }>
-                                    <div>
-                                        <span className={ styles.title }>{ t("myCertificates.belongsTo", { username: username }) }</span>
-                                        <span className={ styles.count }>{ t("myCertificates.count", { count: myCertificates.length }) }</span>
-                                    </div>
-                                    <button data-button-animation={true} type="button" className={ styles.copyButton }>
-                                        <span>{ t("myCertificates.shareYourList") }</span>
-                                        <IconShare/>
-                                    </button>
-                                </div>
-                                <div className={ styles. category }>
-                                    <SortButton>{ t("myCertificates.all") }</SortButton>
-                                    <SortButton>{ t("myCertificates.diploma") }</SortButton>
-                                    <SortButton>{ t("myCertificates.contest") }</SortButton>
-                                    <SortButton>{ t("myCertificates.license") }</SortButton>
-                                    <SortButton>{ t("myCertificates.others") }</SortButton>
-                                </div>
-                            </div>
-                            <div className={ styles.certificates }>
-                                {/*<Collection*/}
-                                {/*    image="/assets/photo/photo-ai-blockchain-education.png"*/}
-                                {/*    href="https://honamict.kr/front/M0000151/program/programRequest.do?pgmId=PM000091"*/}
-                                {/*    category="Education Program"*/}
-                                {/*    date="2023.05.31"*/}
-                                {/*    publisher="Gwangju ICT Innovation Square · Goorm"*/}
-                                {/*    title="A Blockchain Education"*/}
-                                {/*/>*/}
-                            </div>
-                        </div>
-                    ) : (
-                        <EmptyContainer hasSession={true}/>
-                    )
-                }
+                <EmptyContainer hasSession={true}/>
+                {/*{*/}
+                {/*    myCertificates.length > 0 ? (*/}
+                {/*        <div>*/}
+                {/*            <div className={ styles.controller }>*/}
+                {/*                <div className={ styles.header }>*/}
+                {/*                    <div>*/}
+                {/*                        <span className={ styles.title }>{ t("myCertificates.belongsTo", { username: username }) }</span>*/}
+                {/*                        <span className={ styles.count }>{ t("myCertificates.count", { count: myCertificates.length }) }</span>*/}
+                {/*                    </div>*/}
+                {/*                    <button data-button-animation={true} type="button" className={ styles.copyButton }>*/}
+                {/*                        <span>{ t("myCertificates.shareYourList") }</span>*/}
+                {/*                        <IconShare/>*/}
+                {/*                    </button>*/}
+                {/*                </div>*/}
+                {/*                <div className={ styles. category }>*/}
+                {/*                    <SortButton>{ t("myCertificates.all") }</SortButton>*/}
+                {/*                    <SortButton>{ t("myCertificates.diploma") }</SortButton>*/}
+                {/*                    <SortButton>{ t("myCertificates.contest") }</SortButton>*/}
+                {/*                    <SortButton>{ t("myCertificates.license") }</SortButton>*/}
+                {/*                    <SortButton>{ t("myCertificates.others") }</SortButton>*/}
+                {/*                </div>*/}
+                {/*            </div>*/}
+                {/*            <div className={ styles.certificates }>*/}
+                {/*                <Collection*/}
+                {/*                    image="/assets/photo/photo-ai-blockchain-education.png"*/}
+                {/*                    href="https://honamict.kr/front/M0000151/program/programRequest.do?pgmId=PM000091"*/}
+                {/*                    category="Education Program"*/}
+                {/*                    date="2023.05.31"*/}
+                {/*                    publisher="Gwangju ICT Innovation Square · Goorm"*/}
+                {/*                    title="A Blockchain Education"*/}
+                {/*                />*/}
+                {/*            </div>*/}
+                {/*        </div>*/}
+                {/*    ) : (*/}
+                {/*        <EmptyContainer hasSession={true}/>*/}
+                {/*    )*/}
+                {/*}*/}
             </div>
         </TopBar>
     );
