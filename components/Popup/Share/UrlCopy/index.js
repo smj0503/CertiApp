c0d5583b08d4ff1c8f74578f0631c7b649ee0c08
smj0503 <wovj0503@gmail.com>
2024-03-06 18:26:31 +0900

    [UI Design : Sony] 공유 팝업 추가

--- components/Popup/Share/UrlCopy/index.js
+++ components/Popup/Share/UrlCopy/index.js
@@ -2,10 +2,15 @@ import styles from "./UrlCopy.module.css";
 
 export default function ({ url })
 {
+    const copy = () =>
+    {
+
+    };
+
     return (
         <div className={ styles.urlContainer }>
             <span className={ styles.url }>{ url }</span>
-            <button type="button" className={ styles.copyButton } data-button-animation={true}>{ "복사" }</button>
+            <button type="button" className={ styles.copyButton } data-button-animation={true} onClick={ copy }>{ "복사" }</button>
         </div>
     )
 }
