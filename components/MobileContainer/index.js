2533204f226b827eae9b18f3a84bc666704d2e17
smj0503 <wovj0503@gmail.com>
2024-04-22 14:34:22 +0900

    [UI Design : Sony] 로그인 화면 리팩토링

--- components/MobileContainer/index.js
+++ components/MobileContainer/index.js
@@ -1,10 +1,17 @@
-import { Flex } from "antd";
+import { Flex } from 'antd';
 import styles from './MobileContainer.module.css';
 
 export default function ({ children }) {
-    return (
-        <Flex justify='center' align='center' className={styles.container}>
-            <Flex vertical align='center' justify='center' className={styles.mobileContainer}>{children}</Flex>
-        </Flex>
-    );
+  return (
+    <Flex justify='center' align='center' className={styles.container}>
+      <Flex
+        vertical
+        align='center'
+        justify='center'
+        className={styles.mobileContainer}
+      >
+        {children}
+      </Flex>
+    </Flex>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,26 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import useTranslation from 'next-translate/useTranslation';
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { getAddress } from '@/apis/klip.signin.api';
-import { asyncEffect } from '@/common/utils';
 import LocalStorage from '@/common/localstorage.manager';
+
+import { Flex, Button } from 'antd';
 
 import QRCode from '@/components/QRCode';
 
-import MobileContainer from "@/components/MobileContainer";
+import MobileContainer from '@/components/MobileContainer';
 
 import styles from '../styles/Login.module.css';
 import Logo from '../public/assets/logo/logo-signin.svg';
-import Certi from '../public/assets/photo/photo-certi.png';
 import IconCheck from '../public/assets/icon-check.svg';
 import IconKlip from '../public/assets/icon-klip.svg';
 
 export default function () {
   const { t } = useTranslation('common');
   const router = useRouter();
+  const isMobile = useMediaQuery({ query: '(max-width:500px)' });
 
   const [url, setUrl] = useState('');
   const [address, setAddress] = useState('');
@@ -37,14 +38,16 @@ export default function () {
   //     });
   // }, []);
 
-  asyncEffect(async () => {
+  useEffect(() => {
     console.log('address : ', address);
     console.log('status : ', status);
 
-    if (address && status === 'completed') {
-      LocalStorage.shared.setItem('walletAddress', address);
-      await router.replace({ pathname: `/${address}` });
-    }
+    (async () => {
+      if (address && status === 'completed') {
+        LocalStorage.shared.setItem('walletAddress', address);
+        await router.replace({ pathname: `/${address}` });
+      }
+    })();
   }, [address, status]);
 
   const onClick = async () => {
@@ -64,35 +67,51 @@ export default function () {
 
   return (
     <MobileContainer>
-      <div className={styles.loginContainer}>
+      <Flex
+        vertical
+        align='center'
+        justify='center'
+        className={styles.loginContainer}
+      >
         <Logo />
-        <div className={styles.slogan}>
-          <h1 data-color='blue'>{t('signIn.recordYourAchievement')}</h1>
+        <Flex vertical align='center' className={styles.slogan}>
+          <h1>{t('signIn.recordYourAchievement')}</h1>
           <h1>{t('signIn.rememberThatMoment')}</h1>
-        </div>
-        <IconCheck />
-        <p
-          dangerouslySetInnerHTML={{ __html: t('signIn.opengraph') }}
-          className={styles.description}
-        />
-        <button
+        </Flex>
+        <Flex vertical align='center' gap={8}>
+          <IconCheck />
+          <p
+            dangerouslySetInnerHTML={{ __html: t('signIn.opengraph') }}
+            className={styles.description}
+          />
+        </Flex>
+        {/*<button*/}
+        {/*  data-button-animation={true}*/}
+        {/*  type='button'*/}
+        {/*  className={styles.loginButton}*/}
+        {/*  onClick={onClick}*/}
+        {/*>*/}
+        {/*  <Flex align='center' justify='center' gap={16}>*/}
+        {/*    <IconKlip />*/}
+        {/*    <label className={styles.buttonTitle}>*/}
+        {/*      {t('signIn.signInWithKlip')}*/}
+        {/*    </label>*/}
+        {/*  </Flex>*/}
+        {/*</button>*/}
+
+        {/*For Mobile Login*/}
+        <Link
+          href={url}
           data-button-animation={true}
-          type='button'
           className={styles.loginButton}
-          onClick={onClick}
         >
           <IconKlip />
           <label className={styles.buttonTitle}>
             {t('signIn.signInWithKlip')}
           </label>
-        </button>
-        {/*For Mobile Login*/}
-        {/*<Link href={ url } data-button-animation={true} className={ styles.loginButton }>*/}
-        {/*    <IconKlip/>*/}
-        {/*    <label className={ styles.buttonTitle }>{ t("signIn.signInWithKlip") }</label>*/}
-        {/*</Link>*/}
-      </div>
-      {/*<QRCode url={!!url && url} close={close} isOpened={isOpened} />*/}
+        </Link>
+      </Flex>
+      <QRCode url={!!url && url} close={close} isOpened={isOpened} />
     </MobileContainer>
   );
 }
