e69e541580294a56f09ab7e376ddf6a6abb9cbaa
smj0503 <wovj0503@gmail.com>
2024-04-25 10:42:03 +0900

    [UI Design : Sony] certificate 세부 정보 컴포넌트 작업

--- pages/[address]/index.js
+++ pages/[address]/index.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import useTranslation from 'next-translate/useTranslation';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
@@ -9,8 +10,8 @@ import SharePopup from '@/components/Modal/Share';
 import CertificateInfo from '@/components/CertificateInfo';
 import BlockchainInfo from '@/components/BlockchainInfo';
 
-import styles from '../../styles/MyCertificate.module.css';
-import IconShare from '../../public/assets/icon-share.svg';
+import styles from '@/styles/MyWallet.module.css';
+import IconShare from '@/public/assets/icon-share.svg';
 
 export default function () {
   const { t } = useTranslation('common');
@@ -46,6 +47,12 @@ export default function () {
             </button>
           </Flex>
           <Flex vertical gap={36}>
+            <div className={styles.imageContainer}>
+              <img
+                src='https://m.picturemall.co.kr/web/product/big/202011/9c418fbb88f4aa60a9780c7c871378db.jpg'
+                alt='test image'
+              />
+            </div>
             <CertificateInfo />
             <BlockchainInfo />
           </Flex>
