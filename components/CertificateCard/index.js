a6227b7887ee11422c7da94bad64de1c3904cba8
smj0503 <wovj0503@gmail.com>
2024-05-03 10:57:57 +0900

    [UI Design : Sony] Empty Container 추가

---
+++ components/CertificateCard/index.js
@@ -0,0 +1,27 @@
+import styles from './CertificateCard.module.css';
+const TOP_POSITION = 100;
+
+export default function ({ certificate, idx, index, position, onClick }) {
+
+  return (
+    <div
+      className={styles.card}
+      onClick={() => onClick(certificate, idx)}
+      style={{
+        top:
+          index === -1
+            ? TOP_POSITION * idx
+            : index === idx
+              ? 0
+              : position + 526 + 30 * idx,
+      }}
+    >
+      <div className={styles.imageContainer} data-button-animation={true}>
+        <img
+          src={certificate.certificateImageLink}
+          alt={certificate.certificateName}
+        />
+      </div>
+    </div>
+  );
+}
--- pages/[address]/index.js
+++ pages/[address]/index.js
@@ -9,14 +9,13 @@ import { getCertificateListFromShareLink } from '@/apis/share.api';
 import { Flex } from 'antd';
 import MobileContainer from '@/components/MobileContainer';
 import ShareModal from '@/components/Modal/Share';
+import CertificateCard from '@/components/CertificateCard';
 import CertificateInfo from '@/components/CertificateInfo';
 import BlockchainInfo from '@/components/BlockchainInfo';
 import EmptyContainer from '@/components/EmptyContainer';
 
 import styles from '@/styles/MyWallet.module.css';
 import IconShare from '@/public/assets/icon-share.svg';
-
-const TOP_POSITION = 100;
 
 export default function () {
   const { t } = useTranslation('common');
@@ -32,14 +31,6 @@ export default function () {
   const [position, setPosition] = useState(0);
   const [item, setItem] = useState({});
   const [index, setIndex] = useState(-1);
-
-  // const myCertificates = [
-  //   'https://m.picturemall.co.kr/web/product/big/202011/9c418fbb88f4aa60a9780c7c871378db.jpg',
-  //   'https://newsteacher.chosun.com/site/data/img_dir/2023/08/16/2023081603077_0.jpg',
-  //   'https://m.picturemall.co.kr/web/product/big/202305/b5500a98457584ef495c9165d49fc0f6.jpg',
-  //   'https://mblogthumb-phinf.pstatic.net/20091027_14/mimoffllffll_1256603384691rgPk8_jpg/%C1%F8%C1%D6_mimoffllffll.jpg?type=w800',
-  //   'https://www.penews.co.kr/news/photo/202105/19511_18138_1814.jpg',
-  // ];
 
   useEffect(() => {
     if (isMobile) {
@@ -101,29 +92,14 @@ export default function () {
               {certificates.length > 0 ? (
                 certificates.map((certificate, i) => {
                   return (
-                    <div
+                    <CertificateCard
                       key={i}
-                      className={styles.card}
-                      onClick={() => onSelect(certificate, i)}
-                      style={{
-                        top:
-                          index === -1
-                            ? TOP_POSITION * i
-                            : index === i
-                              ? 0
-                              : position + 526 + 30 * i,
-                      }}
-                    >
-                      <div
-                        className={styles.imageContainer}
-                        data-button-animation={true}
-                      >
-                        <img
-                          src={certificate.certificateImageLink}
-                          alt={certificate.certificateName}
-                        />
-                      </div>
-                    </div>
+                      certificate={certificate}
+                      idx={i}
+                      index={index}
+                      position={position}
+                      onClick={onSelect}
+                    />
                   );
                 })
               ) : (
