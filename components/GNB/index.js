2182ad45a2ee28c3e4bb5a87cb5493997c2570f7
smj0503 <wovj0503@gmail.com>
2024-07-01 12:03:25 +0900

    [ETC : Sony] 이미지 및 아이콘 정리

--- components/GNB/index.js
+++ components/GNB/index.js
@@ -3,12 +3,10 @@ import { useRouter } from 'next/router';
 import { Flex } from 'antd';
 
 import styles from './GNB.module.css';
-import IconHomeFill from '@/public/assets/icon-home-fill.svg';
-import IconHomeEmpty from '@/public/assets/icon-home-empty.svg';
-import IconJobPostingFill from '@/public/assets/icon-job-posting-fill.svg';
-import IconJobPostingEmpty from '@/public/assets/icon-job-posting-empty.svg';
-// import IconProfileFill from '@/public/assets/icon-profile-fill.svg';
-// import IconProfileEmpty from '@/public/assets/icon-profile-empty.svg';
+import IconHomeFill from '@/public/assets/bottom-nav/icon-home-fill.svg';
+import IconHomeEmpty from '@/public/assets/bottom-nav/icon-home-empty.svg';
+import IconJobPostingFill from '@/public/assets/bottom-nav/icon-job-posting-fill.svg';
+import IconJobPostingEmpty from '@/public/assets/bottom-nav/icon-job-posting-empty.svg';
 
 export default function () {
   const { t } = useTranslation('common');
